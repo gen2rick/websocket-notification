ac59cada0f3cda083bdc3b92ff6f6c743c5b0d41
gen2rick <chernyk.roman2005@gmail.com>
2025-07-16 06:06:35 +0900

    add PORT constant

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const PORT = process.env.PORT || 3019;
+
 app.use(express.static("public"));
 
 let connectedUsers = 0;
@@ -30,6 +32,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server is running at http://localhost:3000");
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
